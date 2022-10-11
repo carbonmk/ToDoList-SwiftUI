cb7c9ea334325709e2796b78a0f838d35fdd57b6
Michael Carbone <carbonmk@bc.edu>
2022-10-11 15:30:24 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Michael Carbone on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
